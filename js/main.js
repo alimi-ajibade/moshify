dd8a24a92610ce5e698a04ed529335d83fed16d4
HelloSolo <davidalimi148@gmail.com>
2022-08-30 15:33:27 +0100

    added collapsible component

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let collapsible = document.querySelectorAll('.collapsible');
+collapsible.forEach((item) => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('collapsible--expanded')
+    })
+})
